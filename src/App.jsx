5e45c9280faa159b2e0e55612cd0ea8cfa1cb522
Juan Solar <juansolarsolar@yahoo.com>
2023-12-05 19:21:33 -0300

    board

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const width = 8
+const candyColors = [
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "yellow"
+]
+
+function App() {
+
+
+  const [currentColorArrangement, setCurrentColorArragement] = useState([])
+
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++){
+      const randomColor =  candyColors[Math.floor(Math.random()*candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+    // console.log(randomColorArrangement)
+    setCurrentColorArragement(randomColorArrangement)
+  }
+
+
+
+  useEffect(()=>{
+    createBoard()
+
+  },[])
+  console.log(currentColorArrangement)
+  
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <div className='game'>
+        {
+          currentColorArrangement.map((candyColor, index)=>{
+            return (
+              <img 
+                  key={index}
+                  style={{backgroundColor: candyColor}}
+                  alt={candyColor}
+
+              />
+            )
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
